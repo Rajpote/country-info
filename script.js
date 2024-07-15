868136db869742de285bde6808c2c553455d22e2
Rajpote <rrajpote@gmail.com>
2024-07-15 17:21:58 +0545

    code updated

--- script.js
+++ script.js
@@ -30,77 +30,7 @@ const renderError = function (msg) {
   countriesContainer.insertAdjacentText('beforeend', msg);
   //   countriesContainer.style.opacity = 1;
 };
-///////////////////////////////////////
-// const getCountryData = function (country) {
-//   const request = new XMLHttpRequest();
-//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-//   request.send();
 
-//   request.addEventListener('load', function () {
-//     const [data] = JSON.parse(this.responseText);
-//     console.log(data);
-
-//     const languages = Object.values(data.languages).join(', ');
-//     const currencies = Object.values(data.currencies)
-//       .map(currency => currency.name)
-//       .join(', ');
-
-//     const html = `
-//     <article class="country">
-//         <img class="country__img" src="${data.flags.png}" />
-//     <div class="country__data">
-//         <h3 class="country__name">${data.name.common}</h3>
-//         <h4 class="country__region">${data.region}</h4>
-//         <p class="country__row"><span>👫</span>${(
-//           +data.population / 1000000
-//         ).toFixed(1)} People</p>
-//         <p class="country__row"><span>🗣️</span>${languages}</p>
-//         <p class="country__row"><span>💰</span>${currencies}</p>
-//     </div>
-//     </article>`;
-//     countriesContainer.insertAdjacentHTML('beforeend', html);
-//     countriesContainer.style.opacity = 1;
-//   });
-// };
-
-// getCountryData('nepal');
-// // getCountryData('usa');
-
-/*
-const getCountryAndNeighbour = function (country) {
-    // 1st AJAX call
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-    request.send();
-    
-    request.addEventListener('load', function () {
-        const [data] = JSON.parse(this.responseText);
-        console.log(data);
-        renderCountry(data);
-        
-        // get neighbour countery
-        const neighbour = data.borders?.[0];
-        if (!neighbour) return;
-        
-        // 2nd AJAX call
-        const request2 = new XMLHttpRequest();
-        request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
-        request2.send();
-        
-        request2.addEventListener('load', function () {
-            const [data2] = JSON.parse(this.responseText);
-            console.log(data2);
-            renderCountry(data2, 'neighbour');
-        });
-    });
-};
-
-getCountryAndNeighbour('china');
-*/
-
-// Promise
-// const request = fetch(`https://restcountries.com/v3.1/name/nepal`);
-// console.log(request);
 const getJSON = function (url, errorMsg = 'something went wrong') {
   return fetch(url).then(response => {
     if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
@@ -126,18 +56,6 @@ const getCountryData = function (country) {
     .then(data => {
       console.log('Neighbour data:', data);
       renderCountry(data[0], 'neighbour');
-      //     const neighbour2 = data[0].borders ? data[0].borders[0] : null;
-      //     if (!neighbour2) return;
-
-      //     return fetch(`https://restcountries.com/v3.1/alpha/${neighbour2}`);
-      //   })
-      //   .then(response => {
-      //     if (!response) return;
-      //     return response.json();
-      //   })
-      //   .then(data2 => {
-      //     console.log('Neighbour data:', data2);
-      //     renderCountry(data2[0], 'neighbour');
     })
     .catch(err => {
       console.error(`${err}🚫🚫🚫`);
@@ -148,5 +66,5 @@ const getCountryData = function (country) {
     });
 };
 btn.addEventListener('click', function () {
-  getCountryData('nepal');
+  getCountryData('china');
 });
